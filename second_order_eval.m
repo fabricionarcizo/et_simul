e939a9d6bde4efb188c1c8c45a6858222ad98722
Fabricio Batista Narcizo <fabn@itu.dk>
2019-09-17 18:06:46 +0200

    Second order polynomial

---
+++ second_order_eval.m
@@ -0,0 +1,44 @@
+function gaze = second_order_eval(et, camimg)
+%  second_order_eval  Evaluation function for pupil-CR interpolation
+
+%    Copyright 2019 Fabricio Batista Narcizo and the IT University of Copenhagen
+%
+%    This file is part of eyeinfo_simul.
+%
+%    eyeinfo_simul is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License (version 3) as
+%    published by the Free Software Foundation.
+%
+%    eyeinfo_simul is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    (version 3) along with et_simul in a file called 'COPYING'. If not, see
+%    <http://www.gnu.org/licenses/>.
+
+    % Global variables.
+    global is_compensated;
+    global is_undistorted;
+    global is_glint_normalization;
+
+    % Get the current pupil center
+    pc = camimg{1}.pc;
+
+    % PCCR normalization.
+    if (isempty(is_glint_normalization) || is_glint_normalization)
+        pc = pc - camimg{1}.cr{1};
+    end
+
+    % Eye camera location compensation method.
+    if (~isempty(is_compensated) && is_compensated)
+        pc = pupil_compensation(et, pc);
+    end
+
+    % Eye feature distortion compensation method.
+    if (~isempty(is_undistorted) && is_undistorted)
+        pc = undistort_pupil(et, pc);
+    end
+
+    gaze = et.state.A * [1 pc(1) pc(2) pc(1)*pc(2) pc(1)^2 pc(2)^2 pc(1)^2*pc(2)^2]';
---
+++ second_order_make.m
@@ -0,0 +1,60 @@
+function et=second_order_make()
+%  second_order_make  Creates pupil-CR tracker with second-order interpolation
+%    et = second_order_make() creates an eye tracker 'et' that uses biquadratic
+%    interpolation from the pupil-CR difference vector to screen coordinates.
+
+%    Copyright 2019 Fabricio Batista Narcizo and the IT University of Copenhagen
+%
+%    This file is part of eyeinfo_simul.
+%
+%    eyeinfo_simul is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License (version 3) as
+%    published by the Free Software Foundation.
+%
+%    eyeinfo_simul is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    (version 3) along with et_simul in a file called 'COPYING'. If not, see
+%    <http://www.gnu.org/licenses/>.
+
+    % Global variables
+    global eye_camera_position;
+    global user_position;
+
+    et.calib_func=@second_order_calib;
+    et.eval_func=@second_order_eval;
+
+    % Create the camera
+    et.cameras{1}=camera_make;
+    et.cameras{1}.trans(1:3,1:3)=[1 0 0; 0 0 -1; 0 1 0];
+    if (~isempty(eye_camera_position))
+        et.cameras{1}.trans(1:3,4)=eye_camera_position;
+    end
+    et.cameras{1}.rest_trans=et.cameras{1}.trans;
+    et.cameras{1}.err=0.0;
+    et.cameras{1}.err_type='gaussian';
+    if (~isempty(user_position))
+        et.cameras{1}=camera_point_at(et.cameras{1}, user_position);
+    else
+        et.cameras{1}=camera_point_at(et.cameras{1}, [0 550e-3 350e-3 1]');
+    end
+
+    % Create lights
+    et.lights{1}=light_make;
+    et.lights{1}.pos=[ 200e-3 0 350e-3 1]';
+
+    % Calibration points are at the edges of the monitor, halfway between the 
+    % edges and the center of the monitor
+    et.calib_points=[
+        -200e-3  50e-3;
+              0  50e-3;
+         200e-3  50e-3;
+        -200e-3 200e-3;
+              0 200e-3;
+         200e-3 200e-3;
+        -200e-3 350e-3;
+              0 350e-3;
+         200e-3 350e-3]';
